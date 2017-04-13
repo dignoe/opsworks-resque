dc3202117f4ee613f4c619cb7232b9e6141d4b0c
Chad McGimpsey <chad.mcgimpsey@gmail.com>
2017-04-12 21:53:06 -0600

    added extra attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,5 +6,11 @@ node[:deploy].each do |application, deploy|
   default[:resque][application][:restart_command] = "sudo monit restart -g resque_#{application}_group"
   default[:resque][application][:queue] = node[:resque][:queue]
   default[:resque][application][:workers] = node[:resque][:workers]
+
+  force_override["deploy"][application]["migrate"] = false
   
+end
+
+unless node["opsworks"]["instance"]["layers"].include?("rails-app")
+  force_override[:opsworks][:rails_stack][:restart_command] = "echo 'meow'"
 end
