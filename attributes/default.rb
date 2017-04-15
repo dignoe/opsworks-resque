5bf01b925719b7f013c87872b4c636edb2af5d46
Chad McGimpsey <chad.mcgimpsey@gmail.com>
2017-04-14 20:49:20 -0600

    configure => setup

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,10 +7,12 @@ node[:deploy].each do |application, deploy|
   default[:resque][application][:queue] = node[:resque][:queue]
   default[:resque][application][:workers] = node[:resque][:workers]
 
-  force_override["deploy"][application]["migrate"] = false
+  unless node["opsworks"]["instance"]["layers"].include?("rails-app")
+    force_override["deploy"][application]["migrate"] = false
+  end
   
 end
 
 unless node["opsworks"]["instance"]["layers"].include?("rails-app")
-  force_override[:opsworks][:rails_stack][:restart_command] = "echo 'meow'"
+  force_override[:opsworks][:rails_stack][:restart_command] = "echo 'not restarting rails'"
 end
--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,4 +1,4 @@
-include_recipe "opsworks-resque::configure"
+include_recipe "opsworks-resque::setup"
 
 node[:deploy].each do |application, deploy|
 
